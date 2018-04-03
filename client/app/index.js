c3348a37bca998166c37a7679a37c98805b69149
jsolano813 <jsolano.813@gmail.com>
2018-04-03 03:51:34 -0400

    Changed Theme to Orange and White

--- client/app/index.js
+++ client/app/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {deepOrangeA700} from 'material-ui/styles/colors';
+import {amber900} from 'material-ui/styles/colors';
+
+
 import './index.css';
 import App from './App';
 import LoginButton from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+//colors
+const muiTheme = getMuiTheme({
+	palette: {
+		primary1Color: amber900,
+		accent1Color: deepOrangeA700
+	}
+});
+
 ReactDOM.render(
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
