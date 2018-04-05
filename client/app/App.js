bbd24ee5fdff7ebbd656b96a912f3859988559dc
djmlee <komikon94@gmail.com>
2018-04-05 18:54:43 -0400

    - Integrated BasicLayout (components/reactGridLayout/widgetGrid.js) into App.js - npm install --save react-grid-layout && npm install --save react-resizable

--- client/app/App.js
+++ client/app/App.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Redirect, Switch } from 'react-router-dom';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, BrowserRouter } from "react-router-dom";
+//Misc
 import './App.css';
-
-import Footer from './components/Footer/Footer.js';
+import Footer from './components/Footer/Footer';
 import Home from './components/extra/Home';
 import Screen from './components/extra/Screen'
-import UserHomepage from "./components/UserHomepage/UserHomepage.js";
+import UserHomepage from './components/UserHomepage/UserHomepage';
+import BasicLayout from './components/reactGridLayout/widgetGrid';
 
-import { LoginPopup, RegisterBusinessButton, RegislinkterEmployeeButton} from './components/registrationLogin/loginPopUp';
+//Login Related
+import { LoginPopup, RegisterBusinessButton, RegisterEmployeeButton} from './components/registrationLogin/loginPopUp';
 import BusinessSettings from "./components/BusinessSettings/BusinessSettings";
 import UserSettings from './components/UserSettings/UserSettings';
 import ChangePassword from './components/UserSettings/ChangePassword';
 import AddBusiness from './components/UserSettings/AddBusiness';
-import { Redirect, Switch } from 'react-router-dom';
-
+//Material
 import AppBar from 'material-ui/AppBar';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
 import Dialog from 'material-ui/Dialog';
 import RaisedButton from 'material-ui/RaisedButton';
-
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-
+import Paper from 'material-ui/Paper';
 import FontIcon from 'material-ui/FontIcon';
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
-import Paper from 'material-ui/Paper';
 import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 
-import { BrowserRouter as Router, Route, Link, BrowserRouter } from "react-router-dom";
 
 
 class App extends Component {
@@ -72,6 +72,8 @@ class App extends Component {
           <Route path="/AddBusiness" component={AddBusiness} />
         </Switch>
 
+        <BasicLayout />
+        
         <Footer style={{position: 'absolute', bottom: 0, flex: 1, alignSelf: 'stretch', right: 0, left: 0}} />
 
       </div>
