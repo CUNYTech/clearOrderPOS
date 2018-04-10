671723965481c364aad3617765e4e49f14f0a99a
John Solano <jsolano813>
2018-04-09 20:41:33 -0400

    Change primary &secondary colors

--- client/app/index.js
+++ client/app/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {deepOrangeA700} from 'material-ui/styles/colors';
+import {amber900} from 'material-ui/styles/colors';
+
 import './index.css';
-import App from './App';
-import LoginButton from './App';
+import App from './App.js';
+import LoginButton from './App.js';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
-
 // react-grid-layout related imports
 import { Provider } from 'react-redux';
   //for <Provider store={store} to be used below.
 
+//colors
+const muiTheme = getMuiTheme({
+	palette: {
+		primary1Color: amber900,
+		accent1Color: deepOrangeA700
+	}
+});
+
 ReactDOM.render(
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
