7a1130188cf550592fdfbb08ae2769478996ee03
Dan Naeem <dan.naeem95@gmail.com>
2018-04-21 01:18:08 -0400

    moved all the styles to one file

---
+++ client/app/styles/cardStyle.js
@@ -0,0 +1,76 @@
+const outerBox = {
+  boxSizing: 'border-box',
+  margin: 'auto',
+  width: '75%',
+  height: 'auto',
+  padding: '20px',
+  overflow: 'auto',
+  maxHeight: '100%',
+}
+
+const cardStyle = {
+  boxSizing: 'border-box',
+  border: '5px solid transparent',
+  borderRadius: '15px',
+  height: 'auto',
+  maxHeight: '100%',
+  padding: '20px',
+  textAlign: 'center',
+  backgroundColor: '#C2D0DE',
+  margin: '10px 0px',
+  width: '100%',
+  overflow: 'auto',
+}
+
+const flexColumn = {
+  display: 'flex',
+  flexDirection: 'row',
+}
+
+const column = {
+  margin: 'auto',
+}
+
+const outerBusiness = {
+  boxSizing: 'border-box',
+  border: '5px solid transparent',
+  borderRadius: '15px',
+  margin: '0px',
+  padding: '0px',
+  width: '100%',
+  height: '100%',
+  height: '100vh',
+  maxHeight: '100%',
+  minHeight: '100%',
+  display: 'flex',
+  flexDirection: 'row',
+}
+
+const pane = {
+  boxSizing: 'border-box',
+  border: '5px solid transparent',
+  borderRadius: '15px',
+  height: 'auto',
+  padding: '0px',
+  margin: '0px',
+  textAlign: 'center',
+  width: '50%',
+  minWidth: '50%',
+  maxWidth: '50%',
+  overflow: 'auto',
+}
+
+const wideButton ={
+  margin: 'auto',
+  width: '75%',
+}
+
+export {
+  outerBox,
+  cardStyle,
+  flexColumn,
+  column,
+  outerBusiness,
+  pane,
+  wideButton,
+}
