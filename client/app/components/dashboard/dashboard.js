428f2059498bcd2df418b617da063ad3646f6e9c
Dan Naeem <dan.naeem95@gmail.com>
2018-04-28 16:40:47 -0400

    styled dashboard: created spaces for receipt and dashboard

--- client/app/components/dashboard/dashboard.js
+++ client/app/components/dashboard/dashboard.js
@@ -8,6 +8,7 @@ import muiThemeable from 'material-ui/styles/muiThemeable';
 //Other
 import WidgetsGrid from '../reactGridLayout/WidgetsGrid';
 import { Responsive, WidthProvider } from 'react-grid-layout';
+import Receipt from '../mods/receipt';
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
@@ -16,12 +17,47 @@ const dashboardStyle = {
   color:'white',
 }
 
-const mainGrid = {
+const pos = {
   boxSizing: 'border-box',
   border: '5px solid white',
   borderRadius: '15px',
-  width: '98%',
-  height: '98%',
+  margin: '0px',
+  padding: '0px',
+  width: '100%',
+  height: '100%',
+  height: '100vh',
+  maxHeight: '100%',
+  minHeight: '100%',
+  display: 'flex',
+  flexDirection: 'row',
+  overflow: 'none',
+}
+
+const receipt = {
+  boxSizing: 'border-box',
+  border: '5px solid transparent',
+  borderRadius: '15px',
+  height: 'auto',
+  padding: '5px',
+  margin: '0px',
+  textAlign: 'center',
+  width: '30%',
+  minWidth: '30%',
+  maxWidth: '30%',
+  overflow: 'auto',
+}
+
+const mods = {
+  boxSizing: 'border-box',
+  border: '5px solid transparent',
+  borderRadius: '15px',
+  height: 'auto',
+  padding: '0px',
+  margin: '0px',
+  textAlign: 'center',
+  width: '70%',
+  minWidth: '70%',
+  maxWidth: '70%',
   overflow: 'auto',
 }
 
@@ -30,18 +66,23 @@ export default class dashBoard extends Component {
 
   render() {
     return (
-      <div style={mainGrid} >
-        <ResponsiveReactGridLayout
-          className="wid_Grid"
-          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-          cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-        >
-          <div key="wid_grid" data-grid={{x: 5, y: 0, w:12, h: 5, maxH: 7, static: true}}>
-            <Paper zDepth={5} style={dashboardStyle} rounded={true}>
-                <WidgetsGrid />
-            </Paper>
-          </div>
-        </ResponsiveReactGridLayout>
+      <div style={pos} >
+        <div style={receipt}>
+          <Receipt />
+        </div>
+        <div style={mods}>
+          <ResponsiveReactGridLayout
+            className="wid_Grid"
+            breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+            cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+          >
+            <div key="wid_grid" data-grid={{x: 5, y: 0, w:12, h: 5, maxH: 7, static: true}}>
+              <Paper zDepth={5} style={dashboardStyle} rounded={true}>
+                  <WidgetsGrid />
+              </Paper>
+            </div>
+          </ResponsiveReactGridLayout>
+        </div>
       </div>
     );
   }
