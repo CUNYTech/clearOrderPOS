18f8f3add7d5d3183cf7dc7721d3751f23e9df1a
Sashary Morel <sasharyjoanmorel@gmail.com>
2018-04-29 23:44:25 -0400

    Revert "Tables can now be added / removed / selected"
    This reverts commit 07a6e1ab6b1c288dd4253d914d49b9831dd4cabb.

--- client/app/components/dashboard/dashboard.js
+++ client/app/components/dashboard/dashboard.js
@@ -9,7 +9,6 @@ import muiThemeable from 'material-ui/styles/muiThemeable';
 import WidgetsGrid from '../reactGridLayout/WidgetsGrid';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import Receipt from '../mods/receipt';
-import axios from 'axios';
 
 import {dashReceipt, receiptStyle, receiptHeader, receiptBody, receiptFooter} from '../../styles/cardStyle';
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
@@ -55,26 +54,6 @@ const mods = {
 
 
 export default class dashBoard extends Component {
-
-  constructor(){
-    super();
-    this.state = {
-      categories : {},
-    };
-  }
-
-  componentWillMount(){
-    const categories = this.state;
-
-    axios.get('/business/get-categories')
-      .then((response) => {
-        this.setState({
-          categories : response.data[0].business_items,
-        })
-      })
-      .catch((error) => {
-      })
-  }
 
   render() {
     return (
--- server/routes/business/business_receipt.js
+++ server/routes/business/business_receipt.js
@@ -26,59 +26,20 @@ module.exports = (app) => {
   */
 
   // Get categories will also give us items
-  app.get('/business/get-tables', isAuthenticated, function(req, res) {
+  app.get('/business/get-receipt-items', isAuthenticated, function(req, res) {
     // User is auth, so we'll get the business via his business id
     UserModel.findOne({'email' : req.user.email}, 'business_id', (err, person) => {
       if(err)
          return req.status(500).send({state : constants.USER_NOT_FOUND})
-         
-      BusinessModel.find().where('business_id').equals(person.business_id)
-      .select('business_tables')
-      .find(function(err, array){
-        if(err)
-          return req.status(500).send({state : constants.COMPANY_NOT_FOUND})
-        res.status(200).send(array)
-      });
       
     })
   });
 
-  app.post('/business/add-table', isAuthenticated, [
-    check('table_name')
-      .trim()
-      .isLength({min : 1})
-      .withMessage("A table name must be provided"),
-  ], 
-  function (req, res, next) {
-    const validationErrors = validationResult(req)
-    // Validate that the user has given us some input
-    if(!validationErrors.isEmpty())
-      return res.status(400).send({state : constants.VALID_NOT_APPROVED ,message: validationErrors.mapped()})
-
+  app.post('/business/add-table', isAuthenticated, function(req, res) {
     // User is auth, so we'll get the business via his business id
     UserModel.findOne({'email' : req.user.email}, 'business_id', (err, person) => {
       if(err)
          return req.status(500).send({state : constants.USER_NOT_FOUND})
-
-      // We're looking for a specific business id and adding in a new category
-      BusinessModel.findOne({'business_tables.table_name' : req.body.table_name}, (error, result) => {
-        if(!result)
-        {
-          BusinessModel.findOneAndUpdate({'business_id' : person.business_id},
-          {
-            $push : {
-              business_tables : {"table_name": req.body.table_name}
-            } 
-          },{upsert:true, new : true}, (bError, result) => {
-            if(bError)
-              return res.status(500).send({message : 'There was an error confirming the business',  state : constants.BUSINESS_NOT_FOUND})
-  
-            return res.status(200).send({state : constants.SUCCESS, message : req.body.table_name + ' has been added'})
-          })
-        } else {
-          return res.status(500).send({message : 'Table is already in use'});
-        }
-      })
       
     })
   });
@@ -88,17 +49,7 @@ module.exports = (app) => {
     UserModel.findOne({'email' : req.user.email}, 'business_id', (err, person) => {
       if(err)
          return req.status(500).send({state : constants.USER_NOT_FOUND})
-      BusinessModel.findOneAndUpdate({'business_id' : person.business_id},
-      {
-        $pull : {
-          business_tables : {"table_name": req.body.current_table }
-        } 
-      }, { safe: true, upsert: true }, (bError, result) => {
-        if(bError)
-          return res.status(500).send({message : 'There was an error confirming the business', state : constants.BUSINESS_NOT_FOUND})
-
-        return res.status(200).send({state : constants.SUCCESS})
-      })
+      
     })
   });
 
